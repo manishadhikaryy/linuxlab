cf6a1cf4bfb59f444db7e175f3123667e9795179
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2023-05-02 19:34:39 -0400

    we created a function encryption that perform caeser cipher encryption my shifting the input message

--- mycipher.py
+++ mycipher.py
@@ -17,3 +17,32 @@ def format_output(output):
             row = ""
 
     return(lines + row + "\n")
+    
+def encryption(message, shift):
+    message = message.upper()
+characters = ""
+
+for char in message:
+    if char.isalpha():
+    characters += char
+
+encoded_text = []
+
+for char in characters:
+    char_ascii = ord(char)
+    new_ascii = char_ascii + shift
+    
+    if new_ascii > ord("Z"):
+    result = (new_ascii - ord("Z")) % 26
+    
+    if result == 0:
+        result = 26
+    
+    new_ascii = result + 64
+
+    encoded_text.append(chr(new_ascii))
+
+value = ''.join(encoded_text)
+return format_output(value)
+
+args = sys.argv
