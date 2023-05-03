4aa871d03428b787fa36d3a058b1996cb8332001
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2023-05-02 20:23:45 -0400

    There were some indentations error that were fixed and a new textfile was created named plaintext with the message that was to be ciphered

--- hellop.py
+++ hellop.py
@@ -1,4 +1,5 @@
 import sys
+
 def format_output(output):
     row = ""
     lines = ""
@@ -16,34 +17,34 @@ def format_output(output):
             row = ""
 
     return(lines + row + "\n")
-    
+
 def encryption(message, shift):
     message = message.upper()
     characters = ""
-
+    
     for char in message:
-      if char.isalpha():   
-       characters += char
-
+      if char.isalpha():
+        characters += char
+    
     encoded_text = []
-
+    
     for char in characters:
-        char_ascii = ord(char)
-        new_ascii = char_ascii + shift
+      char_ascii = ord(char)
+      new_ascii = char_ascii + shift
+      
+      if new_ascii > ord("Z"):
+        result = (new_ascii - ord("Z")) % 26
         
-        if new_ascii > ord("Z"):
-          result = (new_ascii - ord("Z")) % 26
+        if result == 0:
+          result = 26
         
-          if result == 0:
-            result = 26
-        
-          new_ascii = result + 64
-
-        encoded_text.append(chr(new_ascii))
+        new_ascii = result + 64
+    
+      encoded_text.append(chr(new_ascii))
 
     value = ''.join(encoded_text)
     return format_output(value)
-
+   
 args = sys.argv
 
 for line in sys.stdin:
